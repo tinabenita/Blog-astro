20b49183bb81f17d35b83f083d33333457133563
tinabenita <tina1717rego@gmail.com>
2024-07-22 12:25:01 +0530

    Light/dark mode added

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: 'Astro Learner Tina | Blog',
+    description: 'My journey learning Astro for the first time',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
